80584cde2f794ded2468a28381b17b1248f99ea8
ピアノねこ PianoCat <kotone.olin1010@gmail.com>
2021-09-27 18:29:52 +0900

    Revert "a"
    This reverts commit 9875bf9229fc35ac1758ecc1869526e8374ec65e.

--- src/lib.rs
+++ src/lib.rs
@@ -7,16 +7,16 @@ mod tests {
 }
 
 mod repunit {
-    pub fn convert(repcount: usize) -> usize {
+    pub fn convert(repnum: usize) -> usize {
         let rep1: String = "1".to_string();
-        let rep2 = rep1.repeat(repcount);
+        let rep2 = rep1.repeat(repnum);
         let repunit: usize = rep2.to_string().parse().unwrap();
         return repunit;
     }
     pub fn restore(repunit: usize) -> usize {
         let rep3: String = repunit.to_string();
         let rep4: &str = &rep3;
-        let repcount: usize = rep4.match_indices("1").count();
-        return repcount;
+        let repnum: usize = rep4.match_indices("1").count();
+        return repnum;
     }
 }
