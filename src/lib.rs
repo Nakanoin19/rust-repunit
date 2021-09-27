6128f14b28224351945117ba7ecb3a783b5592b4
ピアノねこ PianoCat <kotone.olin1010@gmail.com>
2021-09-27 21:11:26 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,17 +6,16 @@ mod tests {
     }
 }
 
-mod repunit {
-    pub fn convert(repcount: usize) -> usize {
-        let rep1: String = "1".to_string();
-        let rep2 = rep1.repeat(repcount);
-        let repunit: usize = rep2.to_string().parse().unwrap();
-        return repunit;
-    }
-    pub fn restore(repunit: usize) -> usize {
-        let rep3: String = repunit.to_string();
-        let rep4: &str = &rep3;
-        let repcount: usize = rep4.match_indices("1").count();
-        return repcount;
-    }
+pub fn convert(repcount: usize) -> usize {
+    let rep1: String = "1".to_string();
+    let rep2 = rep1.repeat(repcount);
+    let repunit: usize = rep2.to_string().parse().unwrap();
+    return repunit;
+}
+
+pub fn restore(repunit: usize) -> usize {
+    let rep3: String = repunit.to_string();
+    let rep4: &str = &rep3;
+    let repcount: usize = rep4.match_indices("1").count();
+    return repcount;
 }
